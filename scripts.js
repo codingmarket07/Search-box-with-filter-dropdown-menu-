76bd606251d0641294ff8fcfa55bcce55b1a80bf
rajeshdn <codingmarket@gmail.com>
2022-02-12 15:14:36 +0530

    files added

---
+++ scripts.js
@@ -0,0 +1,33 @@
+var filter = document.querySelector(".filter");
+
+var dropdown_li_items = document.querySelectorAll(".dropdown ul li label");
+
+var checkbox_items = document.querySelectorAll(".dropdown ul li input");
+
+var filter_text = document.querySelector(".filter .text");
+
+filter.addEventListener("click", function(){
+  filter.classList.toggle("active");
+})
+
+dropdown_li_items.forEach(function(item, index){
+  item.addEventListener("click", function(){
+      dropdown_li_items.forEach(function(li){
+        li.classList.remove("active");
+      });
+    
+      checkbox_items.forEach(function(c_item, c_index){
+        if(index == c_index){
+          var item_text = item.querySelector(".text").innerText;
+    filter_text.innerText = item_text;
+          console.log(item_text);
+          c_item.checked = true;
+        }
+        else{
+           c_item.checked = false;
+        }
+      });
+      item.classList.add("active");
+      filter.classList.remove("active");
+  })
+});
